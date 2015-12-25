5028499db0abdced498cf5f5ea313c1803d8fe46
cuihongguo <cuihongguo@baidu.com>
2015-12-25 17:45:47 +0800

    first commit

---
+++ src/toCase.js
@@ -0,0 +1,12 @@
+function toCamelCase(input, separator) {
+    return input.replace(new RegExp('(?:\\' + (separator || '_') + ')([^\1])', 'g'), function ($0, $1) {
+        return $1.toUpperCase();
+    });
+}
+
+function toSnakeCase(input, separator) {
+    separator = separator || '_';
+    return input.replace(/[A-Z]/g, function ($0) {
+        return separator + $0.toLowerCase();
+    });
+}
